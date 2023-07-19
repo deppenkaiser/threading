f2dfbffd80655e5679ca7bb8443b0d51083aea7b
Dipl.-Ing. (FH) Michael Czybor <contact@log1cworx.at>
2023-07-19 21:11:20 +0200

    ADD namespace

--- include/threading.h
+++ include/threading.h
@@ -3,13 +3,16 @@
 #ifdef LINUX
 #include <pthread.h>
 
-typedef pthread_t thread_handle_t;
-typedef void* thread_return_type_t;
+typedef pthread_t threading_thread_handle_t;
+typedef pthread_mutex_t threading_critical_section_t;
+typedef void* threading_thread_return_type_t;
 
-#define INVALID_THREADHANDLE (pthread_t) NULL
+#define THREADING_INVALID_THREADHANDLE (pthread_t) NULL
 
-#endif
+#endif // LINUX
 
-typedef thread_return_type_t (*thread_function)(void* puser_data);
+typedef threading_thread_return_type_t (*threading_thread_function)(void* puser_data);
 
-thread_handle_t create_thread(thread_function function);
+threading_thread_handle_t threading_create_thread(threading_thread_function function);
+void threading_join_thread(threading_thread_handle_t handle);
+void threading_initialize_critical_section(threading_critical_section_t* pcritical_section);
--- src/threading.c
+++ src/threading.c
@@ -1,10 +1,20 @@
 #include "threading.h"
 
 #ifdef LINUX
-thread_handle_t create_thread(thread_function function)
+threading_thread_handle_t threading_create_thread(threading_thread_function function)
 {
-    thread_handle_t handle = INVALID_THREADHANDLE;
+    threading_thread_handle_t handle = THREADING_INVALID_THREADHANDLE;
     pthread_create(&handle, NULL, function, NULL);
     return handle;
+}
+
+void threading_join_thread(threading_thread_handle_t handle)
+{
+    pthread_join(handle, NULL);
+}
+
+void threading_initialize_critical_section(threading_critical_section_t* pcritical_section)
+{
+    pthread_mutex_init(pcritical_section, NULL);
 }
 #endif
