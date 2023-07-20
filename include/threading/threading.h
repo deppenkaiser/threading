03a53acc38cc105b10a8c232dae179f925050fd2
Dipl.-Ing. (FH) Michael Czybor <contact@log1cworx.at>
2023-07-20 13:43:35 +0200

    DOC remove comment

--- include/threading/threading.h
+++ include/threading/threading.h
@@ -56,6 +56,3 @@ socket_handle_t socket_create_socket();
 bool socket_bind_and_listen(socket_handle_t socket);
 socket_handle_t socket_accept_incomming_connection(socket_handle_t socket);
 void socket_close(socket_handle_t* psocket);
-
-/************************************ string ******************************************/
-
