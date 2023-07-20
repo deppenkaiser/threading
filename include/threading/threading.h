73a9f3cccff7f5db9077ae848759cff184ee7aa6
Dipl.-Ing. (FH) Michael Czybor <contact@log1cworx.at>
2023-07-20 14:05:00 +0200

    REF multishit

--- include/threading/threading.h
+++ include/threading/threading.h
@@ -1,27 +1,20 @@
 #pragma once
 
 #ifdef LINUX
-#include <pthread.h>
-
-typedef void* threading_thread_return_type_t;
-typedef pthread_t threading_thread_handle_t;
-typedef pthread_mutex_t threading_critical_section_t;
-
-#define THREADING_INVALID_THREADHANDLE (pthread_t) NULL
-
+    #include <pthread.h>
+    typedef void* threading_thread_return_type_t;
+    typedef pthread_t threading_thread_handle_t;
+    typedef pthread_mutex_t threading_critical_section_t;
+    #define THREADING_INVALID_THREADHANDLE (pthread_t) NULL
 #endif // LINUX
 
 #ifdef WINDOWS
-
-#include <stdbool.h>
-#include <windows.h>
-
-typedef DWORD threading_thread_return_type_t;
-typedef HANDLE threading_thread_handle_t;
-typedef CRITICAL_SECTION threading_critical_section_t;
-
-#define THREADING_INVALID_THREADHANDLE NULL
-
+    #include <stdbool.h>
+    #include <windows.h>
+    typedef DWORD threading_thread_return_type_t;
+    typedef HANDLE threading_thread_handle_t;
+    typedef CRITICAL_SECTION threading_critical_section_t;
+    #define THREADING_INVALID_THREADHANDLE NULL
 #endif
 
 typedef threading_thread_return_type_t (*threading_thread_function)(void* puser_data);
