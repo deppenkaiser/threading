77e37c83ce9ce0157381fac27c7859a0dc5badd2
Dipl.-Ing. (FH) Michael Czybor <contact@log1cworx.at>
2023-08-18 21:53:14 +0200

    ADD recursive mutex

--- include/threading/threading.h
+++ include/threading/threading.h
@@ -13,5 +13,6 @@ typedef threading_thread_return_type_t (*threading_thread_function)(void* puser_
 threading_thread_handle_t threading_create_thread(threading_thread_function function);
 void threading_join_thread(threading_thread_handle_t handle);
 void threading_initialize_critical_section(threading_critical_section_t* pcritical_section);
+void threading_destroy_critical_section(threading_critical_section_t* pcritical_section);
 void threading_lock_critical_section(threading_critical_section_t* pcritical_section);
 void threading_unlock_critical_section(threading_critical_section_t* pcritical_section);
--- src/threading.c
+++ src/threading.c
@@ -14,7 +14,15 @@ void threading_join_thread(threading_thread_handle_t handle)
 
 void threading_initialize_critical_section(threading_critical_section_t* pcritical_section)
 {
-    pthread_mutex_init(pcritical_section, NULL);
+    pthread_mutexattr_t mutexattr = {0};
+    pthread_mutexattr_init(&mutexattr);
+    pthread_mutexattr_settype(&mutexattr, PTHREAD_MUTEX_RECURSIVE_NP);
+    pthread_mutex_init(pcritical_section, &mutexattr);
+}
+
+void threading_destroy_critical_section(threading_critical_section_t* pcritical_section)
+{
+    pthread_mutex_destroy(pcritical_section);
 }
 
 void threading_lock_critical_section(threading_critical_section_t* pcritical_section)
