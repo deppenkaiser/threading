bcb6130243d406d6cc4d7f2d0dcf2d71367cb988
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2024-07-27 15:52:23 +0200

    CHG guiding and tracking

--- threading.h
+++ threading.h
@@ -3,6 +3,7 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <semaphore.h>
 
 #define THREADING_INVALID_THREADHANDLE (pthread_t) NULL
@@ -13,12 +14,12 @@ typedef sem_t threading_semaphore;
 typedef threading_semaphore* threading_semaphore_t;
 typedef void* (*threading_thread_function)(void* user_data);
 
-enum threading_sleep_resolution
+enum threading_time_resolution
 {
-    TSR_NANO,
-    TSR_MICRO,
-    TSR_MILLI,
-    TSR_SECOND
+    TTR_NANO,
+    TTR_MICRO,
+    TTR_MILLI,
+    TTR_SECOND
 };
 
 pthread_t threading_create_thread(threading_thread_function function, void* user_data);
@@ -28,4 +29,7 @@ void threading_destroy_critical_section(threading_critical_section_t critical_se
 void threading_lock_critical_section(threading_critical_section_t critical_section);
 void threading_unlock_critical_section(threading_critical_section_t critical_section);
 void threading_initialize_semaphore(threading_semaphore_t semaphore, uint32_t value);
-void threading_sleep(enum threading_sleep_resolution mode, uint32_t duration);
+void threading_destroy_semaphore(threading_semaphore_t semaphore);
+void threading_increment_semaphore(threading_semaphore_t semaphore);
+bool threading_wait_semaphore(threading_semaphore_t semaphore, enum threading_time_resolution mode, uint32_t duration);
+void threading_sleep(enum threading_time_resolution mode, uint32_t duration);
